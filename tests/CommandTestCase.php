954021ac9ae30a66b4c02dbf3fb12c852a05be63
Simon Dann <simon@photogabble.co.uk>
2015-12-15 11:21:14 +0000

    Allowing CommandTestCase to take a class or string

--- tests/CommandTestCase.php
+++ tests/CommandTestCase.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Container\Container;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 
 abstract class CommandTestCase extends \PHPUnit_Framework_TestCase
@@ -14,14 +15,22 @@ abstract class CommandTestCase extends \PHPUnit_Framework_TestCase
      */
     public function runCommand( $class, $command, array $arguments = [])
     {
-        $container   = new Container();
         $application = new Application();
-        $application->add($container[$class]);
+        $application->setAutoExit(false);
+
+        if ( $class instanceof Command )
+        {
+            $application->add($class);
+        }else{
+            $container   = new Container();
+            $application->add($container[$class]);
+        }
 
         $command = $application->find($command);
         $commandTester = new CommandTester($command);
+        $arguments = array_merge([ 'command' => $command->getName() ], $arguments);
 
-        $commandTester->execute( array_merge([ 'command' => $command->getName() ], $arguments) );
+        $commandTester->execute( $arguments );
         return $commandTester;
     }
 }

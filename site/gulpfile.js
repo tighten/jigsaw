4247902341ff0c9416b3e22b5beac6d27f4f7c56
Adam Wathan <adam.wathan@gmail.com>
2016-01-08 09:01:18 -0500

    Scaffold new sites by copying a skeleton directory instead of the ugly heredoc strings

---
+++ site/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var elixir = require('laravel-elixir');
+
+elixir.config.assetsPath = 'source/_assets';
+elixir.config.publicPath = 'source/assets';
+
+elixir(function(mix) {
+    mix.sass('main.scss')
+        .exec('jigsaw build', ['./source/**/*', '!./source/_assets/**/*']);
+});
--- src/Console/InitCommand.php
+++ src/Console/InitCommand.php
@@ -14,6 +14,7 @@ class InitCommand extends Command
     public function __construct(Filesystem $files)
     {
         $this->files = $files;
+        $this->base = getcwd();
         parent::__construct();
     }
 
@@ -31,144 +32,16 @@ class InitCommand extends Command
     protected function fire()
     {
         if ($base = $this->input->getArgument('name')) {
-            $this->base = getcwd() . '/' . $base;
-            $this->createBaseDirectory();
+            $this->base .= '/' . $base;
         }
 
-        $this->createSourceFolder();
-        $this->createBaseConfig();
-        $this->createGitIgnore();
-        $this->createMasterLayout();
-        $this->createIndexTemplate();
-        $this->initializeElixir();
-        $this->createAssetsFolders();
-        $this->createStylesheets();
+        $this->scaffoldSite();
 
         $this->info('Site initialized successfully!');
     }
 
-    private function createBaseDirectory()
+    private function scaffoldSite()
     {
-        if (! $this->files->isDirectory($this->base)) {
-            $this->files->makeDirectory($this->base);
-        }
-    }
-
-    private function createSourceFolder()
-    {
-        $this->files->makeDirectory($this->base . '/source');
-    }
-
-    private function createBaseConfig()
-    {
-        $this->files->put($this->base . '/config.php', <<<EOT
-<?php
-
-return [
-    'production' => false,
-];
-
-EOT
-        );
-    }
-
-    private function createGitIgnore()
-    {
-        $this->files->put($this->base . '/.gitignore', <<<EOT
-/build_local/
-/node_modules/
-/vendor/
-
-EOT
-        );
-    }
-
-    private function createMasterLayout()
-    {
-        $this->files->makeDirectory($this->base . '/source/_layouts');
-        $this->files->put($this->base . '/source/_layouts/master.blade.php', <<<EOT
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/assets/css/main.css">
-    </head>
-    <body>
-        @yield('body')
-    </body>
-</html>
-
-EOT
-        );
-    }
-
-    private function createIndexTemplate()
-    {
-        $this->files->put($this->base . '/source/index.blade.php', <<<EOT
-@extends('_layouts.master')
-
-@section('body')
-<h1>Hello world!</h1>
-@endsection
-
-EOT
-        );
-    }
-
-    private function initializeElixir()
-    {
-        $this->createPackageJson();
-        $this->createGulpFile();
-    }
-
-    private function createPackageJson()
-    {
-        $this->files->put($this->base . '/package.json', <<<EOT
-{
-  "private": true,
-  "devDependencies": {
-    "gulp": "^3.8.8"
-  },
-  "dependencies": {
-    "laravel-elixir": "^4.2.0"
-  }
-}
-
-EOT
-        );
-    }
-
-    private function createGulpFile()
-    {
-        $this->files->put($this->base . '/gulpfile.js', <<<EOT
-var gulp = require('gulp');
-var elixir = require('laravel-elixir');
-
-elixir.config.assetsPath = 'source/_assets';
-elixir.config.publicPath = 'source/assets';
-
-elixir(function(mix) {
-    mix.sass('main.scss')
-        .exec('jigsaw build', ['./source/**/*', '!./source/_assets/**/*']);
-});
-
-EOT
-        );
-    }
-
-    private function createAssetsFolders()
-    {
-        $this->files->makeDirectory($this->base . '/source/_assets');
-        $this->files->makeDirectory($this->base . '/source/_assets/sass');
-        $this->files->makeDirectory($this->base . '/source/assets/');
-        $this->files->makeDirectory($this->base . '/source/assets/css');
-    }
-
-    private function createStylesheets()
-    {
-        $this->files->put($this->base . '/source/_assets/sass/main.scss', '');
-        $this->files->put($this->base . '/source/assets/css/main.css', '');
+        $this->files->copyDirectory(__DIR__ . '/../../site', $this->base);
     }
 }

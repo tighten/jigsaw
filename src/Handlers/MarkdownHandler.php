9a4ff23993461e1e171ff425835c071665757e00
Adam Wathan <adam.wathan@gmail.com>
2016-01-29 09:21:45 -0500

    Add support for custom front matter variables

--- src/Handlers/MarkdownHandler.php
+++ src/Handlers/MarkdownHandler.php
@@ -35,20 +35,30 @@ class MarkdownHandler
 
     public function render($file, $data)
     {
-        return $this->temporaryFilesystem->put($this->compileToBlade($file), function ($path) use ($data) {
+        list($frontmatter, $content) = $this->parseFile($file);
+
+        $bladeContent = $this->compileToBlade($frontmatter, $content);
+
+        $data = array_merge($data, $frontmatter);
+
+        return $this->temporaryFilesystem->put($bladeContent, function ($path) use ($data) {
             return $this->viewFactory->file($path, $data)->render();
         }, '.blade.php');
     }
 
-    private function compileToBlade($file)
+    private function parseFile($file)
     {
         $document = $this->parser->parse($file->getContents());
-        $yaml = $document->getYAML();
 
+        return [$document->getYAML(), $document->getContent()];
+    }
+
+    private function compileToBlade($frontmatter, $content)
+    {
         return collect([
-            sprintf("@extends('%s')", $yaml['extends']),
-            sprintf("@section('%s')", $yaml['section']),
-            $document->getContent(),
+            sprintf("@extends('%s')", $frontmatter['extends']),
+            sprintf("@section('%s')", $frontmatter['section']),
+            $content,
             '@endsection',
         ])->implode("\n");
     }

794afca0a1c2ffdb5f9c309355719e748440b6c9
Adam Wathan <adam.wathan@gmail.com>
2016-03-09 13:25:07 -0500

    Inject markdown content as variable instead of rendering flat into Blade template

--- src/Handlers/MarkdownHandler.php
+++ src/Handlers/MarkdownHandler.php
@@ -37,9 +37,11 @@ class MarkdownHandler
     {
         $document = $this->parseFile($file);
 
-        $bladeContent = $this->compileToBlade($document);
+        $data = array_merge($data, $document->getYAML(), [
+            '__jigsawMarkdownContent' => $document->getContent()
+        ]);
 
-        $data = array_merge($data, $document->getYAML());
+        $bladeContent = $this->compileToBlade($document->getYAML()['extends'], $document->getYAML()['section']);
 
         return $this->temporaryFilesystem->put($bladeContent, function ($path) use ($data) {
             return $this->viewFactory->file($path, $data)->render();
@@ -51,14 +53,12 @@ class MarkdownHandler
         return $this->parser->parse($file->getContents());
     }
 
-    private function compileToBlade($document)
+    private function compileToBlade($extends, $section)
     {
-        $frontmatter = $document->getYAML();
-
         return collect([
-            sprintf("@extends('%s')", $frontmatter['extends']),
-            sprintf("@section('%s')", $frontmatter['section']),
-            $document->getContent(),
+            sprintf("@extends('%s')", $extends),
+            sprintf("@section('%s')", $section),
+            '{!! $__jigsawMarkdownContent !!}',
             '@endsection',
         ])->implode("\n");
     }

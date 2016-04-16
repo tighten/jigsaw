9d3874208fa79b022ac9e5de4cf1b0dd1b19a805
Jon Sugar <jonathansugar@mac.com>
2016-04-16 13:19:59 +0100

    Allow Browsersync to build all sites not just the local one.

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
+var gutils = require('gulp-util');
 
 elixir.config.assetsPath = 'source/_assets';
 elixir.config.publicPath = 'source';
 
+var $environment = gutils.env.env || 'local';
+
 elixir(function(mix) {
     mix.sass('main.scss')
-        .exec('jigsaw build', ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+        .exec('jigsaw build --env=' + $environment, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
-            server: { baseDir: 'build_local' },
+            server: { baseDir: 'build_' + $environment },
             proxy: null,
-            files: [ 'build_local/**/*' ]
+            files: [ 'build_' + $environment + '/**/*' ]
         });
 });

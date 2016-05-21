46ab27320173597be72854fcffdb54c76635e7f6
Aaron Francis <Aarondfrancis@gmail.com>
2016-05-21 15:31:17 -0500

    Allow Browser Sync port to be specified
    Defaults to the default Browser Sync port: 3000.

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -7,10 +7,12 @@ elixir.config.publicPath = 'source';
 
 elixir(function(mix) {
     var env = argv.e || argv.env || 'local';
+    var port = argv.p || argv.port || 3000;
 
     mix.sass('main.scss')
         .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
+            port: port,
             server: { baseDir: 'build_' + env },
             proxy: null,
             files: [ 'build_' + env + '/**/*' ]

d8111271438c466a23e40ca6cbb32bf154d51ae7
Joshua Romero <josh@oddyssey.io>
2016-06-15 16:05:20 -0400

    Adding the $hidden parameter to the allFiles() method to squelch PHP warnings (#49)

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -5,7 +5,7 @@ use Symfony\Component\Finder\Finder;
 
 class Filesystem extends BaseFilesystem
 {
-    public function allFiles($directory)
+    public function allFiles($directory, $hidden = false)
     {
         return iterator_to_array(Finder::create()->ignoreDotFiles(false)->files()->in($directory), false);
     }

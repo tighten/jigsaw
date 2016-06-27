80c01dd3ea85d1de902deb0e7c64db475c516c48
Josh <joshwhatk@gmail.com>
2016-06-27 12:17:19 -0400

    Add Elixir versioning (#54)

---
+++ src/Support/helpers.php
@@ -0,0 +1,45 @@
+<?php
+
+if (! function_exists('public_path')) {
+    /**
+     * Get the path to the public folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function public_path($path = '')
+    {
+        return 'source'.($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
+
+if (! function_exists('elixir')) {
+    /**
+     * Get the path to a versioned Elixir file.
+     *
+     * @param  string  $file
+     * @param  string  $buildDirectory
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    function elixir($file, $buildDirectory = 'build')
+    {
+        static $manifest;
+        static $manifestPath;
+
+        if (is_null($manifest) || $manifestPath !== $buildDirectory) {
+            $manifest = json_decode(file_get_contents(public_path($buildDirectory.'/rev-manifest.json')), true);
+
+            $manifestPath = $buildDirectory;
+        }
+
+        if (isset($manifest[$file])) {
+            return '/'.trim($buildDirectory.'/'.$manifest[$file], '/');
+        }
+
+        return $file;
+
+        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+    }
+}

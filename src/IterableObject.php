03c076f363d794d132ce28ba47f5fb93fe02ef08
Keith Damiani <keith.damiani@gmail.com>
2016-09-02 10:37:47 -0400

    Throw meaningful error message for missing variables referenced in view

--- src/IterableObject.php
+++ src/IterableObject.php
@@ -1,6 +1,7 @@
 <?php namespace TightenCo\Jigsaw;
 
 use ArrayAccess;
+use Exception;
 use Illuminate\Support\Collection as BaseCollection;
 
 class IterableObject extends BaseCollection implements ArrayAccess
@@ -18,6 +19,11 @@ class IterableObject extends BaseCollection implements ArrayAccess
 
     public function offsetGet($key)
     {
+        if (! isset($this->items[$key])) {
+            $prefix = $this->_source ? 'Error in ' . $this->_source . ': '  : 'Error: ';
+            throw new Exception($prefix . "The key '$key' does not exist.");
+        }
+
         $element = $this->items[$key];
 
         return is_array($element) ? new self($element) : $element;

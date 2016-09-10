21dfba17685ef744d749e021ec0cf3d40e07436a
Leonardo Nodari <nodarileonardo@gmail.com>
2016-09-10 22:15:59 +0200

    Compile any file ending with .blade.xxx

--- src/Handlers/BladeHandler.php
+++ src/Handlers/BladeHandler.php
@@ -7,6 +7,10 @@ class BladeHandler
 {
     private $viewFactory;
 
+    protected $extensions = [
+        'php' => 'html',
+    ];
+
     public function __construct(Factory $viewFactory)
     {
         $this->viewFactory = $viewFactory;
@@ -14,13 +18,28 @@ class BladeHandler
 
     public function canHandle($file)
     {
-        return ends_with($file->getFilename(), '.blade.php');
+        return preg_match('@(.+).blade.(\w+)@', $file->getFilename());
     }
 
     public function handle($file, $data)
     {
-        $filename = $file->getBasename('.blade.php') . '.html';
+        $filename = $this->getCompiledName($file);
         return new ProcessedFile($filename, $file->getRelativePath(), $this->render($file, $data));
+    }
+
+    private function getCompiledName($file)
+    {
+        preg_match('@(.+)\.blade\.(\w+)@', $file->getFilename(), $matches);
+        return $matches[1] . '.' . $this->mutateExtension($matches[2]);
+    }
+
+    private function mutateExtension($extension)
+    {
+        if (isset($this->extensions[$extension])) {
+            return $this->extensions[$extension];
+        }
+
+        return $extension;
     }
 
     public function render($file, $data)

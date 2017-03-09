22d826b4c9eb93f80c4998af1f4e85e1867f0495
Keith Damiani <keith.damiani@gmail.com>
2017-03-09 13:47:39 -0500

    Add ConfigFile class, convert string-type collections to arrays

---
+++ src/ConfigFile.php
@@ -0,0 +1,23 @@
+<?php namespace TightenCo\Jigsaw;
+
+class ConfigFile
+{
+    public $config;
+
+    public function __construct($file_path)
+    {
+        $this->config = file_exists($file_path) ? include $file_path : [];
+        $this->convertStringCollectionsToArray();
+    }
+
+    protected function convertStringCollectionsToArray()
+    {
+        $collections =  array_get($this->config, 'collections');
+
+        if ($collections) {
+            $this->config['collections'] = collect($collections)->flatMap(function ($value, $key) {
+                return is_array($value) ? [$key => $value] : [$value => []];
+            });
+        }
+    }
+}

b7953e38f580e58559744f7f08a48fdf5c9db034
Erick Patrick <contato@erickpatrick.net>
2017-03-23 20:13:53 +0000

    Allow gulp command to check which jigsaw binary to use
    When running gulp, instead of making the user change the gulp file, manually,
    add a check for which jigsaw binary to use (global or local under vendor
    folder) and in the case none exists prints a message to stdout via
    console.log and exits the process.

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 var argv = require('yargs').argv;
+var fs = require('fs')
 
 elixir.config.assetsPath = 'source/_assets';
 elixir.config.publicPath = 'source';
@@ -8,9 +9,15 @@ elixir.config.publicPath = 'source';
 elixir(function(mix) {
     var env = argv.e || argv.env || 'local';
     var port = argv.p || argv.port || 3000;
+    var bin = fs.existsSync('jigsaw') || fs.existsSync('./vendor/bin/jigsaw');
+
+    if (!bin) {
+        console.log('Please, install jigsaw either globally or locally');
+	process.exit()
+    }
 
     mix.sass('main.scss')
-        .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+        .exec(bin + ' build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             port: port,
             server: { baseDir: 'build_' + env },

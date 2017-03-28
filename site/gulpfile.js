9e44f422f96d8b4436205a8010e7054d02d10c22
Erick Patrick <github@erickpatrick.net>
2017-03-28 13:06:59 +0200

    Allow gulp command to check which jigsaw binary to use (#85)
    * Allow gulp command to check which jigsaw binary to use
    
    When running gulp, instead of making the user change the gulp file, manually,
    add a check for which jigsaw binary to use (global or local under vendor
    folder) and in the case none exists prints a message to stdout via
    console.log and exits the process.
    
    * Change path discovery process to a custom local node module
    
    Instead of polluting gulpfile with extra code, move the path
    discovery to its own custom local node module which will
    be required in the gulpfile.
    
    The reasoning behind this was to have a more robust path locator.
    
    * Removed dependency on fs for package.json as it is part of Nodejs core

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 var argv = require('yargs').argv;
+var bin = require('./tasks/bin');
 
 elixir.config.assetsPath = 'source/_assets';
 elixir.config.publicPath = 'source';
@@ -10,7 +11,7 @@ elixir(function(mix) {
     var port = argv.p || argv.port || 3000;
 
     mix.sass('main.scss')
-        .exec('jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+        .exec(bin.path() + ' build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             port: port,
             server: { baseDir: 'build_' + env },
@@ -18,3 +19,4 @@ elixir(function(mix) {
             files: [ 'build_' + env + '/**/*' ]
         });
 });
+

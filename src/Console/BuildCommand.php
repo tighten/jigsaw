3d90a269eb09caa04dde0f76ea2047e5db9fab18
Adam Kelso <akelso@resultsgeneration.com>
2017-07-27 16:57:58 -0500

    Adding multi-site build and configs for each

--- src/Console/BuildCommand.php
+++ src/Console/BuildCommand.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use TightenCo\Jigsaw\ConfigFile;
 use TightenCo\Jigsaw\Jigsaw;
 use TightenCo\Jigsaw\PathResolvers\PrettyOutputPathResolver;
 
@@ -20,6 +21,7 @@ class BuildCommand extends Command
         $this->setName('build')
             ->setDescription('Build your site.')
             ->addArgument('env', InputArgument::OPTIONAL, "What environment should we use to build?", 'local')
+			->addOption('site', 's', InputOption::VALUE_OPTIONAL, "Build a specific site.", null)
             ->addOption('pretty', null, InputOption::VALUE_REQUIRED, "Should the site use pretty URLs?", 'true');
     }
 
@@ -27,15 +29,47 @@ class BuildCommand extends Command
     {
         $env = $this->input->getArgument('env');
         $this->includeEnvironmentConfig($env);
-        $this->updateBuildPaths($env);
 
         if ($this->input->getOption('pretty') === 'true') {
             $this->app->instance('outputPathResolver', new PrettyOutputPathResolver);
         }
 
-        $this->app->make(Jigsaw::class)->build($env);
-        $this->info('Site built successfully!');
+        if(!$this->app->config->has('sites')) {
+        	$this->buildSite($env);
+        	return;
+		}
+
+		$site = $this->input->getOption('site');
+
+        if(!empty($site)) {
+			if(!array_key_exists($site, $this->app->config['sites'])) {
+				$this->error("The site {$site} was not found in the config file's sites array.");
+				return;
+			}
+
+        	$this->buildSite($env, $site, $this->app->config['sites'][$site]);
+        	return;
+		}
+
+		$sites = $this->app->config['sites'];
+		foreach($sites as $name => $source) {
+			$this->buildSite($env, $name, $source);
+		}
     }
+
+    private function buildSite($env, $site = null, $source = null) {
+		if(!empty($site)) {
+			$config = (new ConfigFile($this->getAbsolutePath($source.'/config.php')))->config;
+			$config['build']['source'] = $source.DIRECTORY_SEPARATOR.'source';
+			$this->app->instance('config', collect($config));
+		} else {
+			$site = 'Site';
+		}
+
+		$this->updateBuildPaths($env);
+    	$this->app->make(Jigsaw::class)->build($env);
+    	$this->info("{$site} built successfully.");
+	}
 
     private function includeEnvironmentConfig($env)
     {

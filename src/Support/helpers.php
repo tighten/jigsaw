3c7a8a70b7863ed167d4e1781c32f6cf2fe01ea5
Keith Damiani <keith.damiani@gmail.com>
2017-08-01 23:22:20 -0400

    Add path trim helpers (which include Windows backslashes)

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,43 +1,49 @@
 <?php
 
-if (! function_exists('public_path')) {
-    /**
-     * Get the path to the public folder.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    function public_path($path = '')
-    {
-        return 'source'.($path ? '/'.$path : $path);
-    }
+/**
+ * Remove slashes (including backslashes on Windows),
+ * spaces, and periods from the beginning and/or end of paths.
+ */
+function leftTrimPath($path)
+{
+    return ltrim($path, " .\\/");
 }
 
-if (! function_exists('elixir')) {
-    /**
-     * Get the path to a versioned Elixir file.
-     *
-     * @param  string  $file
-     * @param  string  $buildDirectory
-     * @return string
-     *
-     * @throws \InvalidArgumentException
-     */
-    function elixir($file, $buildDirectory = 'build')
-    {
-        static $manifest;
-        static $manifestPath;
+function rightTrimPath($path)
+{
+    return rtrim($path, " .\\/");
+}
 
-        if (is_null($manifest) || $manifestPath !== $buildDirectory) {
-            $manifest = json_decode(file_get_contents(public_path($buildDirectory.'/rev-manifest.json')), true);
+function trimPath($path)
+{
+    return rightTrimPath(leftTrimPath($path));
+}
 
-            $manifestPath = $buildDirectory;
-        }
+/**
+ * Get the path to the public folder.
+ */
+function public_path($path = '')
+{
+    return 'source' . ($path ? '/' . $path : $path);
+}
 
-        if (isset($manifest[$file])) {
-            return '/'.trim($buildDirectory.'/'.$manifest[$file], '/');
-        }
+/**
+ * Get the path to a versioned Elixir file.
+ */
+function elixir($file, $buildDirectory = 'build')
+{
+    static $manifest;
+    static $manifestPath;
 
-        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+    if (is_null($manifest) || $manifestPath !== $buildDirectory) {
+        $manifest = json_decode(file_get_contents(public_path($buildDirectory . '/rev-manifest.json')), true);
+
+        $manifestPath = $buildDirectory;
     }
+
+    if (isset($manifest[$file])) {
+        return '/' . trim($buildDirectory . '/' . $manifest[$file], '/');
+    }
+
+    throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
 }

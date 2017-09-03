abbfd87e707327d6240f95a1075dcc4cd1ca6b90
Keith Damiani <keith.damiani@gmail.com>
2017-09-03 09:26:48 -0400

    Update webpack script and dependencies

--- site/webpack.mix.js
+++ site/webpack.mix.js
@@ -1,51 +1,50 @@
-let mix = require('laravel-mix');
 let argv = require('yargs').argv;
-let bin = require('./tasks/bin');
-let WebpackShellPlugin = require('webpack-shell-plugin');
+let command = require('node-cmd');
+let jigsaw = require('./tasks/bin');
+let mix = require('laravel-mix');
 
-let env = argv.e || argv.env || 'local';
-var port = argv.p || argv.port || 3000;
+let AfterBuild = require('on-build-webpack');
+let BrowserSync = require('browser-sync');
+let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+let Watch = require('webpack-watch');
 
-mix.setPublicPath('build_' + env);
-mix.webpackConfig({
-    plugins: [
-        new WebpackShellPlugin({ onBuildEnd: [bin.path() + ' build ' + env] })
-    ]
-});
+const env = argv.e || argv.env || 'local';
+const port = argv.p || argv.port || 3000;
+const buildPath = 'build_' + env + '/';
 
-mix.sass('source/_assets/sass/main.scss', '/css/')
-    .browserSync({
-        port: port,
-        server: { baseDir: 'build_' + env },
+let browserSyncInstance;
+
+let plugins = [
+    new AfterBuild(() => {
+        command.get(jigsaw.path() + ' build ' + env, (error, stdout, stderr) => {
+            console.log(error ? stderr : stdout);
+            browserSyncInstance.reload();
+        });
+    }),
+
+    new BrowserSyncPlugin({
         proxy: null,
-        files: [ global.options.publicPath + '/**/*' ]
-    });
+        port: port,
+        server: { baseDir: buildPath },
+        notify: false,
+    },
+    {
+        reload: false,
+        callback: function() {
+            browserSyncInstance = BrowserSync.get('bs-webpack-plugin');
+        },
+    }),
 
-// Full API
-// mix.js(src, output);
-// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
-// mix.extract(vendorLibs);
-// mix.sass(src, output);
-// mix.less(src, output);
-// mix.stylus(src, output);
-// mix.browserSync('my-site.dev');
-// mix.combine(files, destination);
-// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
-// mix.copy(from, to);
-// mix.copyDirectory(fromDir, toDir);
-// mix.minify(file);
-// mix.sourceMaps(); // Enable sourcemaps
-// mix.version(); // Enable versioning.
-// mix.disableNotifications();
-// mix.setPublicPath('path/to/public');
-// mix.setResourceRoot('prefix/for/resource/locators');
-// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
-// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
-// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
-// mix.options({
-//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
-//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
-//   purifyCss: false, // Remove unused CSS selectors.
-//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-// });
+    new Watch({
+        paths: ['source/**/*.md', 'source/**/*.php'],
+        options: { ignoreInitial: true }
+    }),
+];
+
+mix.webpackConfig({ plugins });
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/');
+
+mix.js('source/_assets/js/main.js', 'js/')
+    .sass('source/_assets/sass/main.scss', 'css/')
+    .version();

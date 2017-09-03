82e9bfc354fc8852f9fa35f694b8ead86a7f3303
Keith Damiani <keith.damiani@gmail.com>
2017-09-03 09:27:09 -0400

    Add mix() helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,43 +1,74 @@
 <?php
 
-if (! function_exists('public_path')) {
-    /**
-     * Get the path to the public folder.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    function public_path($path = '')
-    {
-        return 'source'.($path ? '/'.$path : $path);
-    }
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
+
+/**
+ * Get the path to the public folder.
+ */
+function public_path($path = '')
+{
+    return 'source' . ($path ? '/' . $path : $path);
 }
 
-if (! function_exists('elixir')) {
-    /**
-     * Get the path to a versioned Elixir file.
-     *
-     * @param  string  $file
-     * @param  string  $buildDirectory
-     * @return string
-     *
-     * @throws \InvalidArgumentException
-     */
-    function elixir($file, $buildDirectory = 'build')
-    {
-        static $manifest;
-        static $manifestPath;
+/**
+ * Get the path to a versioned Elixir file.
+ */
+function elixir($file, $buildDirectory = 'build')
+{
+    static $manifest;
+    static $manifestPath;
 
-        if (is_null($manifest) || $manifestPath !== $buildDirectory) {
-            $manifest = json_decode(file_get_contents(public_path($buildDirectory.'/rev-manifest.json')), true);
+    if (is_null($manifest) || $manifestPath !== $buildDirectory) {
+        $manifest = json_decode(file_get_contents(public_path($buildDirectory . '/rev-manifest.json')), true);
 
-            $manifestPath = $buildDirectory;
-        }
-
-        if (isset($manifest[$file])) {
-            return '/'.trim($buildDirectory.'/'.$manifest[$file], '/');
-        }
-
-        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+        $manifestPath = $buildDirectory;
     }
+
+    if (isset($manifest[$file])) {
+        return '/' . trim($buildDirectory . '/' . $manifest[$file], '/');
+    }
+
+    throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+}
+
+/**
+ * Get the path to a versioned Mix file.
+ */
+function mix($path, $manifestDirectory = 'assets')
+{
+    static $manifests = [];
+
+    if (! Str::startsWith($path, '/')) {
+        $path = "/{$path}";
+    }
+
+    if ($manifestDirectory && ! Str::startsWith($manifestDirectory, '/')) {
+        $manifestDirectory = "/{$manifestDirectory}";
+    }
+
+    if (file_exists(public_path($manifestDirectory.'/hot'))) {
+        return new HtmlString("//localhost:8080{$path}");
+    }
+
+    $manifestPath = public_path($manifestDirectory.'/mix-manifest.json');
+
+    if (! isset($manifests[$manifestPath])) {
+        if (! file_exists($manifestPath)) {
+            throw new Exception('The Mix manifest does not exist.');
+        }
+
+        $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+    }
+
+    $manifest = $manifests[$manifestPath];
+
+    if (! isset($manifest[$path])) {
+        report(new Exception("Unable to locate Mix file: {$path}."));
+        if (! app('config')->get('app.debug')) {
+            return $path;
+        }
+    }
+
+    return new HtmlString($manifestDirectory.$manifest[$path]);
 }

8322ffdacd32ffcd7bc050929e085602e81c0883
Wilbur Powery <wilburpowery@gmail.com>
2017-10-05 19:07:42 -0600

    Configure the command name, arguments and options

--- src/Console/MakePostCommand.php
+++ src/Console/MakePostCommand.php
@@ -19,6 +19,13 @@ class MakePostCommand extends Command
 
     protected function configure()
     {
+        $this->setName('make:post')
+            ->setDescription('Create a new post factory')
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the file you want to create')
+            ->addOption('title', 't', InputOption::VALUE_OPTIONAL, 'The name of title of the post.', 'Your Own Title')
+            ->addOption('description', 'd', InputOption::VALUE_OPTIONAL, 'The description of title of the post.', 'Your Own Description')
+            ->addOption('layout', 'l', InputOption::VALUE_OPTIONAL, 'The layout your file should extend from', '_layouts.master')
+            ->addOption('section', 's', InputOption::VALUE_OPTIONAL, 'The name of the section you file should use.', 'content');
     }
 
     protected function fire()

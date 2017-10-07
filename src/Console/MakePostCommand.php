5638ac9a99034f11779dfb4f90d897dd7f97c393
Wilbur Powery <wilburpowery@gmail.com>
2017-10-07 15:23:32 -0600

    show an error message when creating a file with a name that already exists

--- src/Console/MakePostCommand.php
+++ src/Console/MakePostCommand.php
@@ -5,19 +5,16 @@ namespace TightenCo\Jigsaw\Console;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use TightenCo\Jigsaw\File\Filesystem;
+use InvalidArgumentException;
 
 class MakePostCommand extends Command
 {
     private $files;
-    private $base;
 
     public function __construct(Filesystem $files)
     {
         $this->files = $files;
-        $this->base = getcwd();
         parent::__construct();
-
-        var_dump($this->base);
     }
 
     protected function configure()
@@ -25,71 +22,78 @@ class MakePostCommand extends Command
         $this->setName('make:post')
             ->setDescription('Create a new post factory')
             ->addArgument('name', InputArgument::REQUIRED, 'The name of the file you want to create')
-            ->addOption('title', 't', InputOption::VALUE_OPTIONAL, 'The name of title of the post.', 'Your Own Title')
-            ->addOption('description', 'd', InputOption::VALUE_OPTIONAL, 'The description of title of the post.', 'Your Own Description')
+            ->addOption('title', 't', InputOption::VALUE_OPTIONAL, 'The name of title of the post.', 'My New Post')
+            ->addOption('description', 'd', InputOption::VALUE_OPTIONAL, 'The description of title of the post.', 'My New Description')
             ->addOption('layout', 'l', InputOption::VALUE_OPTIONAL, 'The layout your file should extend from', '_layouts.master')
-            ->addOption('section', 's', InputOption::VALUE_OPTIONAL, 'The name of the section you file should use.', 'content');
+            ->addOption('section', 's', InputOption::VALUE_OPTIONAL, 'The name of the section you file should use.', 'content')
+            ->addOption('extension', 'e', InputOption::VALUE_OPTIONAL, 'The type of file you want to generate.', 'md');
     }
 
     protected function fire()
     {
-        $file = $this->files->getFile(__DIR__.'/stubs', '*.md');
+        $this->generateFile();
+    }
 
-        $this->updateLayoutKey($file)->updateSectionKey($file);
+    private function getStub()
+    {
+        return $this->files->getFile(__DIR__."/stubs", "*.md");
+    }
+
+    private function getFileExtension()
+    {
+        if (! in_array($this->input->getOption('extension'), ['md', 'blade.md'])) {
+            throw new InvalidArgumentException('Invalid file type');
+        }
+
+        return ".".$this->input->getOption('extension');
+    }
+
+    private function getFileName()
+    {
+        return str_slug(strtolower($this->input->getArgument('name'))) . $this->getFileExtension();
+    }
+
+    private function getFilePath()
+    {
+        $directory = public_path("_posts");
+
+        if (! $this->files->isDirectory($directory)) {
+            $this->files->makeDirectory($directory);
+        }
+
+        return $directory . '/' . $this->getFileName();
+    }
+
+    private function buildContent($stub)
+    {
+        $layout = $this->input->getOption('layout');
+        $title = $this->input->getOption('title');
+        $description = $this->input->getOption('description');
+        $section = $this->input->getOption('section');
+
+        $stubContent = $this->files->get($stub);
+
+        $fileContent = str_replace(
+                ['DummyLayout', 'DummyTitle', 'DummyDescription', 'DummySection'],
+                [$layout, $title, $description, $section],
+                $stubContent
+        );
+
+        return $fileContent;
+    }
+
+    private function generateFile()
+    {
+        if ($this->files->exists($this->getFilePath())) {
+            $this->error('File already exists');
+            return false;
+        }
+
+        $stub = $this->getStub();
+        $content = $this->buildContent($stub);
+
+        $this->files->put($this->getFilePath(), $content);
 
         $this->info("File created.");
-    }
-
-    /**
-     * Apply the needed fixes to the filename.
-     *
-     * @param  string $fileName
-     * @return string
-     */
-    private function normalizeFileName($fileName)
-    {
-        return strtolower($fileName);
-    }
-
-    /**
-     * Update the layout key in the template file.
-     *
-     * @param  File $file
-     * @return $this
-     */
-    private function updateLayoutKey($file)
-    {
-        $this->updateKey($file, 'DummyLayout', '_layouts.master');
-
-        return $this;
-    }
-
-    /**
-     * Update the section key in the template file.
-     *
-     * @param  File $file
-     * @return $this
-     */
-    private function updateSectionKey($file)
-    {
-        $this->updateKey($file, 'DummySection', 'content');
-
-        return $this;
-    }
-
-    /**
-     * Update a given key name with a given value
-     * @param  File $file
-     * @param  string $name
-     * @param  string $value
-     * @return void
-     */
-    private function updateKey($file, $name, $value)
-    {
-        $contents = $this->files->get($file);
-
-        $updated = str_replace($name, $value, $contents);
-
-        file_put_contents($file->getRealPath(), $updated);
     }
 }

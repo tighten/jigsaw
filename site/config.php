970a94e0252828f992e00b44656a16d9c41637a3
Darryl Norris <admin@darrylnorris.com>
2017-10-23 13:11:59 -0700

    Adding metatags into master template.

--- site/config.php
+++ site/config.php
@@ -4,4 +4,8 @@ return [
     'baseUrl' => '',
     'production' => false,
     'collections' => [],
+    'title' => 'Default Page title',
+    'description' => 'Default Page Description',
+    'keywords' => 'Default, Page, Keywords',
+    'lang' => 'en'
 ];

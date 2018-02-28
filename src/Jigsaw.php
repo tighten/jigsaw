ac4ad90ea9609187fa043a79ec83804d5c8a3e8c
Keith Damiani <keith@tighten.co>
2018-02-28 17:11:07 -0500

    Build remote collection items in Jigsaw.php

--- src/Jigsaw.php
+++ src/Jigsaw.php
@@ -4,25 +4,29 @@ class Jigsaw
 {
     private $app;
     private $dataLoader;
+    private $remoteItemLoader;
     private $siteBuilder;
 
-    public function __construct($app, $dataLoader, $siteBuilder)
+    public function __construct($app, $dataLoader, $remoteItemLoader, $siteBuilder)
     {
         $this->app = $app;
         $this->dataLoader = $dataLoader;
+        $this->remoteItemLoader = $remoteItemLoader;
         $this->siteBuilder = $siteBuilder;
     }
 
     public function build($env)
     {
         $siteData = $this->dataLoader->loadSiteData($this->app->config);
-        $collectionData = $this->dataLoader->loadCollectionData($siteData,$this->app->buildPath['source']);
+        $this->remoteItemLoader->write($siteData->collections, $this->app->buildPath['source']);
+        $collectionData = $this->dataLoader->loadCollectionData($siteData, $this->app->buildPath['source']);
 
         $this->siteBuilder->build(
             $this->app->buildPath['source'],
             $this->app->buildPath['destination'],
-            $siteData,
-            $collectionData
+            $siteData->addCollectionData($collectionData)
         );
+
+        $this->remoteItemLoader->cleanup();
     }
 }

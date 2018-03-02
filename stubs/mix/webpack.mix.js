976c11e7a9217a4a20a5d0a002456314771c9ae1
Keith Damiani <keith@tighten.co>
2018-03-02 09:42:08 -0500

    Simplify webpack.mix.js, move plugins to tasks/build.js

--- stubs/mix/webpack.mix.js
+++ stubs/mix/webpack.mix.js
@@ -1,53 +1,16 @@
-let argv = require('yargs').argv;
-let command = require('node-cmd');
-let jigsaw = require('./tasks/bin');
 let mix = require('laravel-mix');
+let build = require('./tasks/build.js');
 
-let AfterBuild = require('on-build-webpack');
-let BrowserSync = require('browser-sync');
-let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-let Watch = require('webpack-watch');
-
-const env = argv.e || argv.env || 'local';
-const port = argv.p || argv.port || 3000;
-const buildPath = 'build_' + env + '/';
-
-let browserSyncInstance;
-
-let plugins = [
-    new AfterBuild(() => {
-        command.get(jigsaw.path() + ' build ' + env, (error, stdout, stderr) => {
-            console.log(error ? stderr : stdout);
-
-            if (browserSyncInstance) {
-                browserSyncInstance.reload();
-            }
-        });
-    }),
-
-    new BrowserSyncPlugin({
-        proxy: null,
-        port: port,
-        server: { baseDir: buildPath },
-        notify: false,
-    },
-    {
-        reload: false,
-        callback: function() {
-            browserSyncInstance = BrowserSync.get('bs-webpack-plugin');
-        },
-    }),
-
-    new Watch({
-        paths: ['source/**/*.md', 'source/**/*.php'],
-        options: { ignoreInitial: true }
-    }),
-];
-
-mix.webpackConfig({ plugins });
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/');
+mix.webpackConfig({
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.sass']),
+    ]
+});
 
-mix.js('source/_assets/js/main.js', 'js/')
-    .sass('source/_assets/sass/main.scss', 'css/')
+mix.js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/less/main.sass', 'css/main.scss')
     .version();

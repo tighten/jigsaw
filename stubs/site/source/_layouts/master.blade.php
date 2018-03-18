a3f44daa33b888964e80ee53b1fc80ee65f72d6a
Josh Moreno <josh@joshmoreno.com>
2018-03-18 15:23:18 -0700

    add css and js in scaffold head, remove old css dir

--- stubs/site/source/_layouts/master.blade.php
+++ stubs/site/source/_layouts/master.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/assets/css/main.css">
+        <script src="/assets/js/main.js"></script>
     </head>
     <body>
         @yield('body')

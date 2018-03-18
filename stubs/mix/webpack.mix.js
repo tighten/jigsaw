dd71292d2cc75c29c9a0dce17d12225ba1978ad0
Josh Moreno <josh@joshmoreno.com>
2018-03-18 15:22:14 -0700

    integrate @joshmoreno/jigsaw

--- stubs/mix/webpack.mix.js
+++ stubs/mix/webpack.mix.js
@@ -1,16 +1,21 @@
-let mix = require('laravel-mix');
-let build = require('./tasks/build.js');
+const mix = require('laravel-mix');
+const jigsaw = require('@joshmoreno/jigsaw');
 
 mix.disableSuccessNotifications();
-mix.setPublicPath('source/assets/');
-mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss']),
-    ]
+
+if (mix.inProduction()) {
+    mix.setPublicPath('build_production/assets/');
+} else {
+    mix.setPublicPath('source/assets/');
+}
+
+mix.webpackConfig(webpack => {
+    return {
+        plugins: [jigsaw.browserSync()]
+    }
 });
 
-mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css/main.css')
-    .version();
+mix.js('source/_assets/js/main.js', 'js/')
+    .sass('source/_assets/sass/main.scss', 'css/');
+
+jigsaw.watch();

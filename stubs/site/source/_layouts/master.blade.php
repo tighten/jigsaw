9ac9692f9c96d5fac60116490bc1124d99e65288
Josh Moreno <josh@joshmoreno.com>
2018-03-19 18:14:25 -0700

    add mix versioning to master.blade

--- stubs/site/source/_layouts/master.blade.php
+++ stubs/site/source/_layouts/master.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/assets/css/main.css">
-        <script src="/assets/js/main.js"></script>
+        <link rel="stylesheet" href="{{mix('css/main.css')}}">
+        <script src="{{mix('js/main.js')}}"></script>
     </head>
     <body>
         @yield('body')

fa65f7cb84f2248f714a5aaccd7d4d9d07bd1be5
Keith Damiani <keith@tighten.co>
2018-03-28 05:55:21 -0500

    Style fixes

--- src/File/BladeDirectivesFile.php
+++ src/File/BladeDirectivesFile.php
@@ -4,25 +4,19 @@ use Illuminate\View\Compilers\BladeCompiler;
 
 class BladeDirectivesFile
 {
-    /** @var BladeCompiler */
     protected $bladeCompiler;
-
-    /** @var array */
     protected $directives;
 
-    /**
-     * @param string $file_path Path to file containing array of blade directives
-     * @param BladeCompiler $bladeCompiler
-     */
     public function __construct($file_path, BladeCompiler $bladeCompiler)
     {
         $this->bladeCompiler = $bladeCompiler;
         $this->directives = file_exists($file_path) ? include $file_path : [];
-        if (!is_array($this->directives)) {
+
+        if (! is_array($this->directives)) {
             $this->directives = [];
         }
     }
-    
+
     public function register()
     {
         collect($this->directives)->each(function ($callback, $directive) {

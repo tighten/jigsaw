9a38fed171d94a499c31a6cea3a0e3945f5d3204
Keith Damiani <keith@tighten.co>
2018-04-17 09:20:20 -0400

    Clarify Jigsaw variable/event syntax in bootstrap.php

--- stubs/site/bootstrap.php
+++ stubs/site/bootstrap.php
@@ -1,5 +1,17 @@
 <?php
 
+use TightenCo\Jigsaw\Jigsaw;
+
 /** @var $container \Illuminate\Container\Container */
 /** @var $events \TightenCo\Jigsaw\Events\EventBus */
-/** @var $jigsaw \TightenCo\Jigsaw\Jigsaw */
+
+/**
+ * You can run custom code at different stages of the build process by
+ * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
+ *
+ * For example:
+ *
+ * $events->beforeBuild(function (Jigsaw $jigsaw) {
+ *     // Your code here
+ * });
+ */

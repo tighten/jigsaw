771dca0da4ddd6278eb67ee089f032c963a31071
Ranie Santos <ranietots32@gmail.com>
2018-08-25 21:12:06 +0800

    Added SEO tags to master.blade.php stub

--- stubs/site/source/_layouts/master.blade.php
+++ stubs/site/source/_layouts/master.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ $page->language ?? 'en' }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="referrer" content="always">
+        <link rel="canonical" href="{{ $page->getUrl() }}">
+        <meta name="description" content="{{ $page->description }}">
+        <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body>

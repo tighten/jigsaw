f079fb3ff22562d71e84c13248f83fac67314772
Keith Damiani <keith@tighten.co>
2019-03-08 18:01:41 -0500

    Extend ParsedownExtra to fix handling of single-line markup in markdown

---
+++ src/Parsers/JigsawMarkdownParser.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace TightenCo\Jigsaw\Parsers;
+
+use DOMDocument;
+use DOMElement;
+use ParsedownExtra;
+
+class JigsawMarkdownParser extends ParsedownExtra
+{
+    // This extention of ParsedownExtra is intended to deal with parsing errors
+    // that arise from single-line markup that appears in markdown, e.g.
+    //
+    // <h1>Some heading</h1><p>Some content</p>
+    //
+    // Only the first node, <h1> in this example, is maintained.
+    //
+    // This fix comes largely from Adam Mitchell in this PR to ParsedownExtra:
+    // https://github.com/erusev/parsedown-extra/pull/58
+    // ...which has been around since 2015, but has not been merged into the package.
+    //
+    // It has been updated here to work correctly with changes that were made
+    // to Parsedown and Parsedown Extra in the interim. If that PR gets merged someday,
+    // we can probably safely remove this, so long as all tests in MarkdownExtraPass
+    // continue to pass.
+
+    protected $document;
+
+    protected function blockMarkupComplete($Block)
+    {
+        if (! isset($Block['void'])) {
+            $Block['element']['rawHtml'] = $this->processTags($Block['element']['rawHtml']);
+        }
+
+        return $Block;
+    }
+
+    protected function processTags($elementMarkup)
+    {
+        // http://stackoverflow.com/q/1148928/200145
+        libxml_use_internal_errors(true);
+        $DOMDocument = new DOMDocument();
+
+        // http://stackoverflow.com/q/11309194/200145
+        $elementMarkup = mb_convert_encoding($elementMarkup, 'HTML-ENTITIES', 'UTF-8');
+
+        // http://stackoverflow.com/q/4879946/200145
+        $DOMDocument->loadHTML($elementMarkup, LIBXML_HTML_NODEFDTD);
+
+        // This will add wrapping <body> and <html> tags, which we need to ignore.
+        // Trying to _remove_ them, instead, will break single-line, multi-node markup
+        $dom = $DOMDocument->firstChild->firstChild;
+        $markup = '';
+
+        if ($dom->hasChildNodes()) {
+            foreach ($dom->childNodes as $childElement) {
+                if ($childElement->nodeName !== 'html' && $childElement->nodeName !== 'body') {
+                    $markup .= $this->processTag($childElement, $DOMDocument);
+                }
+            }
+        }
+
+        return $markup;
+    }
+
+    protected function processTag($element, $document = null)
+    {
+        $elementText = '';
+
+        if ($element instanceof DOMElement && $element->getAttribute('markdown') === '1') {
+            if ($element->hasChildNodes()) {
+                foreach ($element->childNodes as $node) {
+                    $elementText .= $document->saveHTML($node);
+                }
+            } else {
+                $elementText = $document->saveHTML($element);
+            }
+
+            $element->removeAttribute('markdown');
+            $elementText = "\n" . $this->text($elementText) . "\n";
+        } else {
+            if ($element->hasChildNodes()) {
+                foreach ($element->childNodes as $node) {
+                    $elementText .= $this->processText($node, $document);
+                }
+            } else {
+                $elementText = $this->processText($element, $document);
+            }
+        }
+
+        // because we don't want markup to get encoded
+        $element->nodeValue = 'placeholder\x1A';
+
+        $markup = $document->saveHTML($element);
+        $markup = str_replace('placeholder\x1A', $elementText, $markup);
+
+        return $markup;
+    }
+
+    protected function processText($element, $document)
+    {
+        $nodeMarkup = $document->saveHTML($element);
+        $text = '';
+
+        if ($element instanceof DOMElement
+            && ! in_array($element->nodeName, $this->textLevelElements)
+            && ! in_array($element->nodeName, $this->voidElements)
+        ) {
+            if ($element->hasChildNodes()) {
+                $text = $this->processTags($nodeMarkup);
+            } else {
+                $text = $nodeMarkup;
+            }
+        } else {
+            $text = $nodeMarkup;
+        }
+
+        return $text;
+    }
+}

e3cf59a783917a8b935d9ad62a2cb7c0d4d87972
Matt Loberg <loberg.matt@gmail.com>
2019-10-14 18:10:44 -0500

    run build as part of the serve command

--- src/Console/ServeCommand.php
+++ src/Console/ServeCommand.php
@@ -4,6 +4,7 @@ namespace TightenCo\Jigsaw\Console;
 
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\Arr;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -40,6 +41,12 @@ class ServeCommand extends Command
                 InputOption::VALUE_REQUIRED,
                 'What port should we use?',
                 8000
+            )
+            ->addOption(
+                'no-build',
+                null,
+                InputOption::VALUE_NONE,
+                'Skip build before serving?'
             );
     }
 
@@ -48,6 +55,13 @@ class ServeCommand extends Command
         $env = $this->input->getArgument('environment');
         $host = $this->input->getOption('host');
         $port = $this->input->getOption('port');
+
+        if (! $this->input->getOption('no-build')) {
+            $buildCmd = $this->getApplication()->find('build');
+            $buildArgs = new ArrayInput(['env' => $env]);
+
+            $buildCmd->run($buildArgs, $this->output);
+        }
 
         $this->console->info("Server started on http://{$host}:{$port}");
 

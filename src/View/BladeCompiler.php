f415f512b0a64933501ca3af7428c7b2d2af48ad
Keith Damiani <keith@tighten.co>
2020-03-20 21:46:43 -0400

    Extend BladeCompiler and ComponentTagCompiler

---
+++ src/View/BladeCompiler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TightenCo\Jigsaw\View;
+
+use Illuminate\View\Compilers\BladeCompiler as BaseBladeCompiler;
+use TightenCo\Jigsaw\View\ComponentTagCompiler;
+
+class BladeCompiler extends BaseBladeCompiler
+{
+    /**
+     * Compile the component tags.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function compileComponentTags($value)
+    {
+        if (! $this->compilesComponentTags) {
+            return $value;
+        }
+
+        return (new ComponentTagCompiler(
+            $this->classComponentAliases
+        ))->compile($value);
+    }
+}

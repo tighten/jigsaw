6b424b0c371385d2aa57c330bc69442ee084a5e9
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-04-10 17:23:28 -0400

    Update webpack.mix.js stub to use `laravel-mix-jigsaw`

--- stubs/mix/webpack.mix.js
+++ stubs/mix/webpack.mix.js
@@ -1,18 +1,13 @@
-let mix = require('laravel-mix');
-let build = require('./tasks/build.js');
+const mix = require('laravel-mix');
+require('laravel-mix-jigsaw');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
-mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-    ]
-});
 
-mix.js('source/_assets/js/main.js', 'js')
+mix.jigsaw()
+    .js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
         processCssUrls: false,
-    }).version();
+    })
+    .version();

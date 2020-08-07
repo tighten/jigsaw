e329ede8d406a9f4a14d0f2aa6136ca84b25af56
dorian <dorian@mino.rs>
2020-08-07 17:28:08 +0100

    add filter to ignore '.git' in CollectionDataLoader.php

--- src/Loaders/CollectionDataLoader.php
+++ src/Loaders/CollectionDataLoader.php
@@ -60,7 +60,7 @@ class CollectionDataLoader
 
         return collect($this->filesystem->files($path))
             ->reject(function ($file) {
-                return Str::startsWith($file->getFilename(), '_');
+                return Str::startsWith($file->getFilename(), '_') || Str::startsWith($file->getFilename(), '.git');
             })->tap(function ($files) {
                 $this->consoleOutput->progressBar('collections')->addSteps($files->count());
             })->map(function ($file) {

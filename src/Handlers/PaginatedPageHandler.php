42fae56ad119b29eb5c7ea149c91a8bf538048e5
Craig Anderson <craig.anderson@intrepidws.com>
2021-02-09 11:59:55 -0500

    Allow collection specific perPage setting

--- src/Handlers/PaginatedPageHandler.php
+++ src/Handlers/PaginatedPageHandler.php
@@ -46,7 +46,11 @@ class PaginatedPageHandler
         return $this->paginator->paginate(
             $file,
             $pageData->get($pageData->page->pagination->collection),
-            $pageData->page->pagination->perPage ?: ($pageData->page->perPage ?: 10)
+            $pageData->page->pagination->perPage ?: (
+                $pageData->page->collections->{$pageData->page->pagination->collection}->perPage ?: (
+                    $pageData->page->perPage ?: 10
+                )
+            )
         )->map(function ($page) use ($file, $pageData) {
             $pageData->setPagePath($page->current);
             $pageData->put('pagination', $page);

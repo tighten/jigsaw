62ebf7428edd467d99b295d6806c7627725d2608
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2022-05-08 20:07:09 -0300

    Adicionando o verificação se existe a função.

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -6,108 +6,125 @@ use Illuminate\Support\HtmlString;
 use Illuminate\Support\Str;
 use Symfony\Component\VarDumper\VarDumper;
 
-function leftTrimPath($path)
-{
-    return ltrim($path, ' \\/');
+if (!function_exists('leftTrimPath')) {
+    function leftTrimPath($path)
+    {
+        return ltrim($path, ' \\/');
+    }
 }
 
-function rightTrimPath($path)
-{
-    return rtrim($path ?? '', ' .\\/');
+if (!function_exists('rightTrimPath')) {
+    function rightTrimPath($path)
+    {
+        return rtrim($path ?? '', ' .\\/');
+    }
 }
 
-function trimPath($path)
-{
-    return rightTrimPath(leftTrimPath($path));
+if (!function_exists('trimPath')) {
+    function trimPath($path)
+    {
+        return rightTrimPath(leftTrimPath($path));
+    }
 }
 
-function resolvePath($path)
-{
-    $path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
-    $segments = [];
+if (!function_exists('resolvePath')) {
+    function resolvePath($path)
+    {
+        $path = str_replace([
+            '/',
+            '\\',
+        ], DIRECTORY_SEPARATOR, $path);
+        $segments = [];
 
-    collect(explode(DIRECTORY_SEPARATOR, $path))->filter()->each(function ($part) use (&$segments) {
-        if ($part == '..') {
-            array_pop($segments);
-        } elseif ($part != '.') {
-            $segments[] = $part;
-        }
-    });
+        collect(explode(DIRECTORY_SEPARATOR, $path))->filter()->each(function ($part) use (&$segments) {
+            if ($part == '..') {
+                array_pop($segments);
+            } elseif ($part != '.') {
+                $segments[] = $part;
+            }
+        });
 
-    return implode(DIRECTORY_SEPARATOR, $segments);
+        return implode(DIRECTORY_SEPARATOR, $segments);
+    }
 }
 
 /**
  * Get the path to the public folder.
  */
-function public_path($path = '')
-{
-    $c = Container::getInstance();
-    $source = Arr::get($c['config'], 'build.source', 'source');
+if (!function_exists('public_path')) {
+    function public_path($path = '')
+    {
+        $c = Container::getInstance();
+        $source = Arr::get($c['config'], 'build.source', 'source');
 
-    return $source . ($path ? '/' . ltrim($path, '/') : $path);
+        return $source . ($path ? '/' . ltrim($path, '/') : $path);
+    }
 }
 
 /**
  * Get the path to a versioned Elixir file.
  */
-function elixir($file, $buildDirectory = 'build')
-{
-    static $manifest;
-    static $manifestPath;
+if (!function_exists('elixir')) {
+    function elixir($file, $buildDirectory = 'build')
+    {
+        static $manifest;
+        static $manifestPath;
 
-    if (is_null($manifest) || $manifestPath !== $buildDirectory) {
-        $manifest = json_decode(file_get_contents(public_path($buildDirectory . '/rev-manifest.json')), true);
+        if (is_null($manifest) || $manifestPath !== $buildDirectory) {
+            $manifest = json_decode(file_get_contents(public_path($buildDirectory . '/rev-manifest.json')), true);
 
-        $manifestPath = $buildDirectory;
+            $manifestPath = $buildDirectory;
+        }
+
+        if (isset($manifest[$file])) {
+            return '/' . trim($buildDirectory . '/' . $manifest[$file], '/');
+        }
+
+        throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
     }
-
-    if (isset($manifest[$file])) {
-        return '/' . trim($buildDirectory . '/' . $manifest[$file], '/');
-    }
-
-    throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
 }
 
 /**
  * Get the path to a versioned Mix file.
  */
-function mix($path, $manifestDirectory = 'assets')
-{
-    static $manifests = [];
+if (!function_exists('mix')) {
+    function mix($path, $manifestDirectory = 'assets')
+    {
+        static $manifests = [];
 
-    if (! Str::startsWith($path, '/')) {
-        $path = "/{$path}";
-    }
-
-    if ($manifestDirectory && ! Str::startsWith($manifestDirectory, '/')) {
-        $manifestDirectory = "/{$manifestDirectory}";
-    }
-
-    if (file_exists(public_path($manifestDirectory . '/hot'))) {
-        return new HtmlString("//localhost:8080{$path}");
-    }
-
-    $manifestPath = public_path($manifestDirectory . '/mix-manifest.json');
-
-    if (! isset($manifests[$manifestPath])) {
-        if (! file_exists($manifestPath)) {
-            throw new Exception('The Mix manifest does not exist.');
+        if (!Str::startsWith($path, '/')) {
+            $path = "/{$path}";
         }
 
-        $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+        if ($manifestDirectory && !Str::startsWith($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+
+        if (file_exists(public_path($manifestDirectory . '/hot'))) {
+            return new HtmlString("//localhost:8080{$path}");
+        }
+
+        $manifestPath = public_path($manifestDirectory . '/mix-manifest.json');
+
+        if (!isset($manifests[$manifestPath])) {
+            if (!file_exists($manifestPath)) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+
+            $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+        }
+
+        $manifest = $manifests[$manifestPath];
+
+        if (!isset($manifest[$path])) {
+            throw new InvalidArgumentException("Unable to locate Mix file: {$path}.");
+        }
+
+        return new HtmlString($manifestDirectory . $manifest[$path]);
     }
-
-    $manifest = $manifests[$manifestPath];
-
-    if (! isset($manifest[$path])) {
-        throw new InvalidArgumentException("Unable to locate Mix file: {$path}.");
-    }
-
-    return new HtmlString($manifestDirectory . $manifest[$path]);
 }
 
-if (! function_exists('url')) {
+if (!function_exists('url')) {
     function url(string $path): string
     {
         $c = Container::getInstance();
@@ -116,7 +133,7 @@ if (! function_exists('url')) {
     }
 }
 
-if (! function_exists('dd')) {
+if (!function_exists('dd')) {
     function dd(...$args)
     {
         foreach ($args as $x) {
@@ -127,15 +144,17 @@ if (! function_exists('dd')) {
     }
 }
 
-function inline($assetPath)
-{
-    preg_match('/^\/assets\/build\/(css|js)\/.*\.(css|js)/', $assetPath, $matches);
+if (!function_exists('inline')) {
+    function inline($assetPath)
+    {
+        preg_match('/^\/assets\/build\/(css|js)\/.*\.(css|js)/', $assetPath, $matches);
 
-    if (!count($matches)) {
-        throw new InvalidArgumentException("Given asset path is not valid: {$assetPath}");
+        if (!count($matches)) {
+            throw new InvalidArgumentException("Given asset path is not valid: {$assetPath}");
+        }
+
+        $pathParts = explode('?', $assetPath);
+
+        return new HtmlString(file_get_contents("source{$pathParts[0]}"));
     }
-
-    $pathParts = explode('?', $assetPath);
-
-    return new HtmlString(file_get_contents("source{$pathParts[0]}"));
 }

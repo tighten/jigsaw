fcf2dc99981da236d80f2500b60bfa88e762c245
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-08-12 15:53:17 -0400

    Revert `ConfigFile` changes
    Co-authored-by: zuzana-kunckova <zk0433@gmail.com>

--- src/File/ConfigFile.php
+++ src/File/ConfigFile.php
@@ -11,33 +11,18 @@ class ConfigFile
         $config = file_exists($config_path) ? include $config_path : [];
         $helpers = file_exists($helpers_path) ? include $helpers_path : [];
 
-        $this->config = $this->convertStringCollectionsToArray(
-            collect($config)->merge($helpers)
-        );
+        $this->config = collect($config)->merge($helpers);
+        $this->convertStringCollectionsToArray();
     }
 
-    protected function convertStringCollectionsToArray($config)
+    protected function convertStringCollectionsToArray()
     {
-        $collections = $config->get('collections');
+        $collections = $this->config->get('collections');
 
         if ($collections) {
-            $config->put('collections', collect($collections)->flatMap(function ($value, $key) {
+            $this->config->put('collections', collect($collections)->flatMap(function ($value, $key) {
                 return is_array($value) ? [$key => $value] : [$value => []];
             }));
         }
-
-        return $config;
-    }
-
-    public static function mergeConfigs($baseConfig, $configToMerge)
-    {
-        return (new static(''))->convertStringCollectionsToArray(
-            array_filter(
-                array_replace_recursive(
-                    collect($baseConfig)->toArray(),
-                    collect($configToMerge)->toArray()
-                )
-            )
-        );
     }
 }

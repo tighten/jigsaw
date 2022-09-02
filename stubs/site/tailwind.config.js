759f72995d6e8956370c26aaf1bea637728cf6f1
Keith Damiani <keith@tighten.co>
2022-09-02 09:50:11 -0400

    Include blade.md files as Tailwind content

--- stubs/site/tailwind.config.js
+++ stubs/site/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
  content: require('fast-glob').sync([
-    'source/**/*.{blade.php,md,html,vue}',
+    'source/**/*.{blade.php,blade.md,md,html,vue}',
     '!source/**/_tmp/*' // exclude temporary files
   ],{ dot: true }),
   theme: {

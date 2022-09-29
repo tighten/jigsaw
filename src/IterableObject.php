d2eb52c4f146e94bff735c184185b244385f9045
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-09-29 14:24:25 -0400

    Add `offsetGet()` return type

--- src/IterableObject.php
+++ src/IterableObject.php
@@ -61,7 +61,7 @@ class IterableObject extends BaseCollection implements ArrayAccess
     }
 
     #[\ReturnTypeWillChange]
-    public function offsetGet($key)
+    public function offsetGet($key): mixed
     {
         if (! isset($this->items[$key])) {
             $prefix = $this->_source ? 'Error in ' . $this->_source . ': ' : 'Error: ';

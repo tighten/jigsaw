cb57be8c09535c4e9d9e01102cd486d9df01f21e
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-09-29 14:23:49 -0400

    Remove unnecessary `guessViewName` method and update `componentClass` method with some upstream changes

--- src/View/ComponentTagCompiler.php
+++ src/View/ComponentTagCompiler.php
@@ -6,7 +6,6 @@ use Illuminate\Container\Container;
 use Illuminate\Support\Str;
 use Illuminate\View\Compilers\ComponentTagCompiler as BaseComponentTagCompiler;
 use Illuminate\View\Factory;
-use Illuminate\View\ViewFinderInterface;
 use InvalidArgumentException;
 
 class ComponentTagCompiler extends BaseComponentTagCompiler
@@ -21,7 +20,7 @@ class ComponentTagCompiler extends BaseComponentTagCompiler
      */
     public function componentClass(string $component)
     {
-        $viewFactory = Container::getInstance()[Factory::class];
+        $viewFactory = Container::getInstance()->make(Factory::class);
 
         if (isset($this->aliases[$component])) {
             if (class_exists($alias = $this->aliases[$component])) {
@@ -37,12 +36,38 @@ class ComponentTagCompiler extends BaseComponentTagCompiler
             );
         }
 
+        if ($class = $this->findClassByComponent($component)) {
+            return $class;
+        }
+
         if (class_exists($class = $this->guessClassName($component))) {
             return $class;
         }
 
-        if ($viewFactory->exists($view = $this->guessViewName($component))) {
-            return $view;
+        $guess = collect($this->blade->getAnonymousComponentNamespaces())
+            ->filter(function ($directory, $prefix) use ($component) {
+                return Str::startsWith($component, $prefix . '::');
+            })
+            // This is the only line that differs from the overidden method, to add the underscore
+            ->prepend('_components', $component)
+            ->reduce(function ($carry, $directory, $prefix) use ($component, $viewFactory) {
+                if (! is_null($carry)) {
+                    return $carry;
+                }
+
+                $componentName = Str::after($component, $prefix . '::');
+
+                if ($viewFactory->exists($view = $this->guessViewName($componentName, $directory))) {
+                    return $view;
+                }
+
+                if ($viewFactory->exists($view = $this->guessViewName($componentName, $directory) . '.index')) {
+                    return $view;
+                }
+            });
+
+        if (! is_null($guess)) {
+            return $guess;
         }
 
         throw new InvalidArgumentException(
@@ -62,24 +87,5 @@ class ComponentTagCompiler extends BaseComponentTagCompiler
             return ucfirst(Str::camel($componentPiece));
         }, explode('.', $component));
         return 'Components\\'.implode('\\', $componentPieces);
-    }
-
-    /**
-     * Guess the view name for the given component.
-     *
-     * @param  string  $name
-     * @return string
-     */
-    public function guessViewName($name)
-    {
-        $prefix = '_components.';
-
-        $delimiter = ViewFinderInterface::HINT_PATH_DELIMITER;
-
-        if (Str::contains($name, $delimiter)) {
-            return Str::replaceFirst($delimiter, $delimiter.$prefix, $name);
-        }
-
-        return $prefix.$name;
     }
 }

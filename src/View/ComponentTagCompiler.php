a671ecd3254e1a5b7bd9878af85f2008f8a9a6fb
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-09-30 10:28:22 -0400

    Remove unnecessary `guessViewName()` method override

--- src/View/ComponentTagCompiler.php
+++ src/View/ComponentTagCompiler.php
@@ -6,7 +6,6 @@ use Illuminate\Container\Container;
 use Illuminate\Support\Str;
 use Illuminate\View\Compilers\ComponentTagCompiler as BaseComponentTagCompiler;
 use Illuminate\View\Factory;
-use Illuminate\View\ViewFinderInterface;
 use InvalidArgumentException;
 
 class ComponentTagCompiler extends BaseComponentTagCompiler
@@ -49,7 +48,7 @@ class ComponentTagCompiler extends BaseComponentTagCompiler
             ->filter(function ($directory, $prefix) use ($component) {
                 return Str::startsWith($component, $prefix . '::');
             })
-            // This is the only line that differs from the overidden method, to add the underscore
+            // This is the only line that differs from the overidden method (to add the leading underscore)
             ->prepend('_components', $component)
             ->reduce(function ($carry, $directory, $prefix) use ($component, $viewFactory) {
                 if (! is_null($carry)) {
@@ -85,25 +84,5 @@ class ComponentTagCompiler extends BaseComponentTagCompiler
     public function guessClassName(string $component)
     {
         return 'Components\\' . $this->formatClassName($component);
-    }
-
-    /**
-     * Guess the view name for the given component.
-     *
-     * @param  string  $name
-     * @param  string  $prefix
-     * @return string
-     */
-    public function guessViewName($name, $prefix = '_components.')
-    {
-        $prefix = Str::finish($prefix, '.');
-
-        $delimiter = ViewFinderInterface::HINT_PATH_DELIMITER;
-
-        if (Str::contains($name, $delimiter)) {
-            return Str::replaceFirst($delimiter, $delimiter . $prefix, $name);
-        }
-
-        return $prefix . $name;
     }
 }

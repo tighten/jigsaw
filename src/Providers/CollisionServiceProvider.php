9b64231de018508bfa18ed5cbee784d895ad1504
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-11-11 17:20:26 -0500

    Install `spatie/laravel-ignition` and map Blade exceptions to show uncompiled views in exception traces

--- src/Providers/CollisionServiceProvider.php
+++ src/Providers/CollisionServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace TightenCo\Jigsaw\Providers;
 
-use NunoMaduro\Collision\Contracts\Provider as ProviderContract;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\View\ViewException;
 use NunoMaduro\Collision\Contracts\Provider as CollisionProviderContract;
 use NunoMaduro\Collision\Provider as CollisionProvider;
+use Spatie\LaravelIgnition\Views\ViewExceptionMapper;
 use TightenCo\Jigsaw\Support\ServiceProvider;
 
 class CollisionServiceProvider extends ServiceProvider
@@ -15,5 +17,9 @@ class CollisionServiceProvider extends ServiceProvider
         // if (! $this->app->runningUnitTests()) {
         $this->app->bind(CollisionProviderContract::class, fn () => new CollisionProvider);
         // }
+
+        $this->app->make(ExceptionHandler::class)->map(
+            fn (ViewException $e) => $this->app->make(ViewExceptionMapper::class)->map($e),
+        );
     }
 }

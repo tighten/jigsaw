4c2eb6cc9e69757cccb8c65a1997c0bdc731a022
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-11-11 15:16:13 -0500

    Use Collision in our own exception handler instead of overwriting it, and add missing command handling

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace TightenCo\Jigsaw\Exceptions;
 
+use Illuminate\Console\View\Components\BulletList;
+use Illuminate\Console\View\Components\Error;
 use Illuminate\Contracts\Debug\ExceptionHandler;
+use NunoMaduro\Collision\Adapters\Laravel\Inspector;
+use NunoMaduro\Collision\Contracts\Provider;
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Exception\ExceptionInterface as SymfonyConsoleExceptionInterface;
 use Throwable;
 
 class Handler implements ExceptionHandler
@@ -18,14 +25,6 @@ class Handler implements ExceptionHandler
     /**
      * {@inheritdoc}
      */
-    public function shouldReport(Throwable $e)
-    {
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function render($request, Throwable $e)
     {
         //
@@ -36,6 +35,43 @@ class Handler implements ExceptionHandler
      */
     public function renderForConsole($output, Throwable $e)
     {
-        //
+        if ($e instanceof CommandNotFoundException) {
+            $message = str($e->getMessage())->explode('.')->first();
+
+            if (! empty($alternatives = $e->getAlternatives())) {
+                $message .= '. Did you mean one of these?';
+
+                with(new Error($output))->render($message);
+                with(new BulletList($output))->render($e->getAlternatives());
+
+                $output->writeln('');
+            } else {
+                with(new Error($output))->render($message);
+            }
+
+            return;
+        }
+
+        if ($e instanceof SymfonyConsoleExceptionInterface) {
+            (new ConsoleApplication)->renderThrowable($e, $output);
+
+            return;
+        }
+
+        /** @var \NunoMaduro\Collision\Contracts\Provider $provider */
+        $provider = app(Provider::class);
+
+        $handler = $provider->register()->getHandler()->setOutput($output);
+        $handler->setInspector(new Inspector($e));
+
+        $handler->handle();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function shouldReport(Throwable $e)
+    {
+        return true;
     }
 }
--- src/Providers/CollisionServiceProvider.php
+++ src/Providers/CollisionServiceProvider.php
@@ -2,13 +2,8 @@
 
 namespace TightenCo\Jigsaw\Providers;
 
-use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
-use NunoMaduro\Collision\Adapters\Laravel\ExceptionHandler;
 use NunoMaduro\Collision\Contracts\Provider as ProviderContract;
-use NunoMaduro\Collision\Handler;
 use NunoMaduro\Collision\Provider;
-use NunoMaduro\Collision\SolutionsRepositories\NullSolutionsRepository;
-use NunoMaduro\Collision\Writer;
 use TightenCo\Jigsaw\Support\ServiceProvider;
 
 class CollisionServiceProvider extends ServiceProvider
@@ -17,19 +12,7 @@ class CollisionServiceProvider extends ServiceProvider
     {
         // TODO bind something global in the TestCase for now?
         // if (! $this->app->runningUnitTests()) {
-            $this->app->bind(ProviderContract::class, function () {
-                $writer = new Writer(new NullSolutionsRepository);
-                $handler = new Handler($writer);
-
-                return new Provider(null, $handler);
-            });
-
-            /** @var \Illuminate\Contracts\Debug\ExceptionHandler $appExceptionHandler */
-            $appExceptionHandler = $this->app->make(ExceptionHandlerContract::class);
-
-            $this->app->singleton(ExceptionHandlerContract::class, function ($app) use ($appExceptionHandler) {
-                return new ExceptionHandler($app, $appExceptionHandler);
-            });
+            $this->app->bind(ProviderContract::class, fn () => new Provider);
         // }
     }
 }

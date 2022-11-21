5b0f75b7a2559656c597a02ca16e2d7d06fbe69f
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-11-21 17:37:55 -0500

    Add ability to configure command name and parameters using a `$signature` property

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -2,15 +2,36 @@
 
 namespace TightenCo\Jigsaw\Console;
 
+use Illuminate\Console\Concerns\HasParameters;
+use Illuminate\Console\Parser;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class Command extends SymfonyCommand
 {
+    use HasParameters;
+
+    /** @var string */
+    protected $signature;
+
     protected $input;
     protected $output;
     protected $console;
+
+    public function __construct()
+    {
+        if (isset($this->signature)) {
+            $this->parseSignature();
+        } else {
+            parent::__construct($this->name);
+
+            $this->specifyParameters();
+        }
+
+        $this->setDescription((string) $this->description);
+        $this->setHelp((string) $this->help);
+    }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
@@ -26,4 +47,14 @@ abstract class Command extends SymfonyCommand
     }
 
     abstract protected function fire();
+
+    private function parseSignature(): void
+    {
+        [$name, $arguments, $options] = Parser::parse($this->signature);
+
+        parent::__construct($this->name = $name);
+
+        $this->getDefinition()->addArguments($arguments);
+        $this->getDefinition()->addOptions($options);
+    }
 }
